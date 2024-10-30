c9831e507a7fc5089d1698a66f959321d9146a21
Mordecai <chingolo265@gmail.com>
2024-10-30 10:21:41 +0200

    getting started page with navigation to homepage functionality

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
 
@@ -8,14 +10,77 @@ class IntroPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body: Center(
-        child: Column(
-          children: [
-            //logo
-            Image.asset(
-              'lib/images/nike.png',
-              height: 240,
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //logo
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Image.asset(
+                  'lib/images/nike.png',
+                  height: 240,
+                ),
+              ),
+
+              const SizedBox(
+                height: 48,
+              ),
+
+              //title
+              const Text(
+                'Just do it',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+
+              const SizedBox(
+                height: 14,
+              ),
+
+              //title
+              const Text(
+                'Brand new Sneakers and custom kicks made with premium quality',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.grey,
+                ),
+                textAlign: TextAlign.center,
+              ),
+
+              const SizedBox(
+                height: 48,
+              ),
+
+              //start now button
+              GestureDetector(
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const HomePage()),
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.grey[900],
+                      borderRadius: BorderRadius.circular(12)),
+                  padding: const EdgeInsets.all(25),
+                  child: const Center(
+                    child: Text(
+                      'Shop Now',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
